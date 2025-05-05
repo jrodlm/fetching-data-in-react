682f680ba222f329f7e48bd5066428bc58a8d385
jrodlm <jarodlmorris@gmail.com>
2025-05-05 19:45:03 -0400

    fetching data in react complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,49 @@
+import * as weatherService from './services/weatherService';
+import './App.css'
+import WeatherSearch from './components/WeatherSearch/WeatherSearch';
+import { useState } from 'react';
+import WeatherDetails from './components/WeatherDetails/WeatherDetails';
+import { useEffect } from 'react';
+
 
 const App = () => {
+    const [weather, setWeather] = useState({});
 
-  return (
-    <h1>Hello world!</h1>
-  );
-}
+    const fetchData = async (city) => {
+      const data = await weatherService.show(city);
+      const newWeatherState = {
+        location: data.location.name,
+        temperature: data.current.temp_f,
+        condition: data.current.condition.text,
+      };
+  setWeather(newWeatherState);
+    };
+
+useEffect(() => {
+
+  const fetchDefaultData = async () => {
+    const data = await weatherService.show('Atlanta');
+    const newWeatherState = {
+      location: data.location.name,
+      temperature: data.current.temp_f,
+      condition: data.current.condition.text,
+    };
+    setWeather(newWeatherState);
+  }
+  fetchDefaultData()
+}, []);
+
+    console.log('State:', weather);
+
+
+    return (
+      <main>
+        <h1>Weather API</h1>
+        <WeatherSearch fetchData={fetchData}/>
+        <WeatherDetails weather={weather}/>
+
+      </main>
+    );
+};
 
 export default App
